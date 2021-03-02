fd99dde60da0c33f5461ae5747ac6d19a21e8aaf
Idiljei <i.jeilani@gmail.com>
2021-03-01 21:09:09 -0700

    Employee creation for stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -14,5 +14,5 @@ puts "----------"
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store1.employees.create(first_name: "Michael", last_name: "Fox", hourly_rate: 30)
 
-@store2.employees.create(first_name: "Whitney", last_name: "Houston", hourly_rate: 1000)
+@store2.employees.create(first_name: "Whitney", last_name: "Houston", hourly_rate: 200)
 @store2.employees.create(first_name: "Mona", last_name: "James", hourly_rate: 40)
