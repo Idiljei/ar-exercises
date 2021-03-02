e6f0064871cae4e038ecf409309b0ace58555236
Idiljei <i.jeilani@gmail.com>
2021-03-01 19:24:33 -0700

    added 3 more stores and updated queries

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,35 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+# Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
+
+# Borrowing and modifying the code from Exercise one, create 3 more stores:
+Store.create(name: "Surrey",
+annual_revenue: 224000,
+mens_apparel: false,
+womens_apparel: true
+)
+puts "----------"
+
+Store.create(name: "Whistler",
+annual_revenue: 1900000,
+mens_apparel: true,
+womens_apparel: false
+)
+puts "----------"
+
+Store.create(name: "Yaletown",
+annual_revenue: 430000,
+mens_apparel: true,
+womens_apparel: true
+)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|  
+ puts "Name: #{store.name} Annual Revenue:#{store.annual_revenue}"
+end
+
+@womens_stores = Store.where(womens_apparel: true).where('annual_revenue < 1000000', true)
+@womens_stores.each do |store| 
+  puts "Name: #{store.name} Annual Revenue:#{store.annual_revenue}"
+end 
