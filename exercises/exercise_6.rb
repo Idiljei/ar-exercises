8f46bf476a066cf496701c34644334b83325ecf7
Idiljei <i.jeilani@gmail.com>
2021-03-01 19:58:39 -0700

    Populated employees table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Michael", last_name: "Fox", hourly_rate: 30)
+
+@store2.employees.create(first_name: "Whitney", last_name: "Houston", hourly_rate: 1000)
+@store2.employees.create(first_name: "Mona", last_name: "James", hourly_rate: 40)
